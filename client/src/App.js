26cea3b190fd7166815e5a4c73e65b97fa79cc42
Tori Miller <millertori04@gmail.com>
2020-09-25 14:23:49 -0700

    created signup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-//import Register from './components/auth/Register';
+import Register from './components/auth/Register';
 //import Login from './components/auth/Login';
 //import LoginDemo from './components/auth/LoginDemo';
 //import Alert from './components/layout/Alert';
@@ -38,7 +38,7 @@ const App = () => {
         <section className="container">
           {/* <Alert /> */}
           <Switch>
-            {/* <Route exact path="/register" component={Register} /> */}
+            <Route exact path="/register" component={Register} />
             {/* <Route exact path="/login" component={Login} /> */}
             {/* <Route exact path="/login-demo" component={LoginDemo} /> */}
             {/* <PrivateRoute exact path="/dashboard" component={Dashboard} /> */}
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { setAlert } from './alert';
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -6,49 +8,96 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGOUT,
-    ACCOUNT_DELETED
-} from '../actions/types';
+    // CLEAR_PROFILE
+} from './types';
+import setAuthToken from '../utils/setAuthToken';
 
-const initialState = {
-    token: localStorage.getItem('token'),
-    isAuthenticated: null,
-    loading: true,
-    user: null
+// Load User
+export const loadUser = () => async dispatch => {
+    // check local storage
+    if(localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
+    try {
+        const res = await axios.get('/api/auth');
+
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
 }
 
-export default function(state = initialState, action) {
-    const { type, payload } = action;
-
-    switch(type) {
-        case USER_LOADED:
-            return {
-                ...state,
-                isAuthenticated: true,
-                loading: false,
-                user: payload
-            }
-        case REGISTER_SUCCESS:
-            localStorage.setItem('token', payload.token);
-            return {
-                ...state,
-                ...payload,
-                isAuthenticated: true,
-                loading: false
-            }
-        case REGISTER_FAIL:
-        case AUTH_ERROR:
-        case LOGIN_FAIL:
-        case LOGOUT:
-        case ACCOUNT_DELETED:
-            console.log('ACCOUNT_DELETED ran in auth reducer')
-            //localStorage.removeItem('token');
-            return {
-                ...state,
-                token: null,
-                isAuthenticated: false,
-                loading: false
-            }
-        default:
-            return state;
+// Register User
+export const register = ({ name, email, password }) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
     }
+
+    const body = JSON.stringify({ name, email, password });
+
+    try {
+        const res = await axios.post('/api/users', body, config);
+
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: REGISTER_FAIL
+        });
+    }
+}
+
+// Login User
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+        const res = await axios.post('/api/auth', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+        localStorage.setItem('token', res.data.token);
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
+        });
+    }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    // dispatch({ type: CLEAR_PROFILE });
+    dispatch({ type: LOGOUT });
 }
