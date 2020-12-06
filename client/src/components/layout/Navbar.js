22f299fced1416fcded1a59ace274a928a8eb0c2
Tori Miller <millertori04@gmail.com>
2020-12-06 00:01:23 -0800

    created navbar layout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,32 +7,48 @@ import '../../App.css';
 
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
-  const authLinks = (
-    <ul>
-      <li>
-        <Link to="/maintenance-options">
-        <i className="fas fa-user" alt=""></i>{' '}
-        <span className="hide-sm">Maintenance Options</span></Link>
-      </li>
-      <li>
-        <a onClick={logout} href="#!">
-          <i className="fas fa-sign-out-alt" alt=""></i>{' '}
-          <span className="hide-sm">Logout</span></a>
-      </li>
-    </ul>
-  );
+  // const authLinks = (
+  //   <ul>
+  //     <li>
+  //       <Link to="/maintenance-options">
+  //       <i className="fas fa-user" alt=""></i>{' '}
+  //       <span className="hide-sm">Maintenance Options</span></Link>
+  //     </li>
+  //     <li>
+  //       <a onClick={logout} href="#!">
+  //         <i className="fas fa-sign-out-alt" alt=""></i>{' '}
+  //         <span className="hide-sm">Logout</span></a>
+  //     </li>
+  //   </ul>
+  // );
 
-  const guestLinks = (
-    <ul>
-      <li><Link to="/register">Register</Link></li>
-      <li><Link to="/login">Login</Link></li>
-    </ul>
-  );
+  // const guestLinks = (
+  //   <ul>
+  //     <li><Link to="/register">Register</Link></li>
+  //     <li><Link to="/login">Login</Link></li>
+  //   </ul>
+  // );
 
 
     return (
     <nav className="navbar">
-      { !loading && (<Fragment>{ isAuthenticated ? authLinks: guestLinks }</Fragment>) }
+      <ul>
+        <li className="nav-link-1">
+          <Link to="/maintenance-options">Maintenance Options</Link>
+        </li>
+        <li className="nav-link-2">
+          <Link to="/pending-maintenance">Pending Maintenance</Link>
+        </li>
+        <li className="nav-link-3">
+          <Link to="/completed-maintenance">Completed Maintenance</Link>
+        </li>
+        <li>
+          <a onClick={logout} href="#!">
+            <i className="fas fa-sign-out-alt" alt=""></i>{' '}
+            <span className="hide-sm">Logout</span></a>
+        </li>
+      </ul>
+      {/* { !loading && (<Fragment>{ isAuthenticated ? authLinks: guestLinks }</Fragment>) } */}
     </nav>
     )
 }
