d97e266cc0bb2bc5abdd2317750fe3df4b6578d4
Tori Miller <millertori04@gmail.com>
2020-12-12 23:52:57 -0800

    removed unnecessary code

--- client/src/components/maintenance/MaintenanceOptions.js
+++ client/src/components/maintenance/MaintenanceOptions.js
@@ -3,8 +3,6 @@ import Navbar from '../layout/Navbar';
 import light from './img/car-light.svg';
 
 export default function MaintenanceOptions() {
-    // store maintenance options in a state list
-    // each maintenance item will be its own component
 
     const monthlyItems = [
         'Check Lights', 
@@ -15,24 +13,33 @@ export default function MaintenanceOptions() {
         'Check coolant'
     ]
 
-    const maintenanceItems = [
-        {
-            monthly: [
-                'Check Lights', 
-                'Tire inflation, tread, and general condition',
-                'Engine oil',
-                'Check windshield wiper fluid',
-                'Check brake fluid',
-                'Check coolant'
-                ],
-            threeMonths: [
-                'Automatic transmission fluid',
-                'Battery and cables',
-                'Check belts and hoses',
-                'Check power steering fluid'
-            ]
-        }
-    ];
+    const threeMonths = [
+        'Automatic transmission fluid',
+        'Battery and cables',
+        'Check belts and hoses',
+        'Check power steering fluid'
+    ]
+
+    const sixMonths = [
+        'Chassis lubrication',
+        'Thorough detail',
+        'Engine air filter',
+        'Check exhaust system',
+        'Replace wiper blades',
+        'Brakes inspection'
+    ]
+
+
+    const twelveMonths = [
+        'Replace cabin air filter',
+        'Check fuel filter',
+        'Check steering and suspension',
+        'Flush brake fluid',
+        'Check alignment',
+        'Polish windshield and headlights'
+    ]
+
+
 
     const [hidden, setHidden] = useState(true);
     const [key, setKey] = useState(null);
@@ -73,112 +80,23 @@ export default function MaintenanceOptions() {
             <h2>Monthly</h2>
             <div className="wrapper-1">
                 {monthlyTasks}
-                {/* <div className="maintenance-item">
-                    <img src={light} className="icon" />
-                    <p className="maintenance-task">Check Lights</p>
-                    <p className="schedule-task">Schedule this task</p>
-                    
-                </div>
-                <div className="maintenance-item">
-                    <img src={light} className="icon" />
-                    <p className="maintenance-task">Tire inflation, tread, and general condition</p>
-                    <p className="schedule-task">Schedule this task</p>
-                    
-                </div>
-                <div className="maintenance-item">
-                    <img src={light} className="icon" />
-                    <p className="maintenance-task">Engine oil</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check windshield wiper fluid</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check brake fluid</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check coolant</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div> */}
             </div>
             
             <h2>Every 3 months/<br/>3,000 miles</h2>
             <div className="wrapper-2">
-                <div className="maintenance-item">
-                    <img src={light} className="icon" />
-                    <p className="maintenance-task">Automatic transmission fluid</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Battery and cables</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check belts and hoses</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check power steering fluid</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
+
             </div>
+
+            
 
             <h2>Every 6 months/<br/>6,000 miles</h2>
             <div className="wrapper-3">
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Chassis lubrication</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Thorough detail</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Engine air filter</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check exhaust system</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Replace wiper blades</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Brakes inspection</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
             </div>
+            
 
             <h2>Every 12 months/<br/>12,000 miles</h2>
             <div className="wrapper-4">
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Replace cabin air filter</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check fuel filter</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check steering and suspension</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Flush brake fluid</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Check alignment</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
-                <div className="maintenance-item">
-                    <p className="maintenance-task">Polish windshield and headlights</p>
-                    <p className="schedule-task">Schedule this task</p>
-                </div>
+        
             </div>
             </section>  
         </Fragment>
